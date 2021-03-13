930763994c9eaa2fef1175f9c0a024170ac34292
kuanyinhsieh192 <79040803+kuanyinhsieh192@users.noreply.github.com>
2021-03-13 22:09:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qgc3uymavtjztjprfhk4knt73ccprl8htwjsldt.test1
+./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1qtyya0lwv6mclnax0hhnlsx0apchavgnjkc2ccj.x
 
 
 done
